eaa6a61b3c97530eca43732e727bccd41c870084
devdutta <devjothi@gmail.com>
2014-10-26 00:02:35 +0530

    Steps 3, 4 and 5 added

--- run_analysis.R
+++ run_analysis.R
@@ -16,12 +16,16 @@
 
 ##    1) This directory is already present --> "UCI HAR Dataset"
 ##    2) All the relevant data files are also present within this directory
-##    3) Expecting that the "reshpae" package is installed. If not, then you will have to install it before
-##    4) running this script
+##    3) Expecting that the "reshpae" package and the plyr package is installed. 
+##    4) If not, then you will have to install it before running this script
 
 run_analysis <- function() {
   
-    
+  ## Since we will be using the reshape and plyr package, invoking them first
+  
+  library(reshape)
+  library(plyr)
+  
   ## Importing the Test Data
   
   X_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
@@ -39,7 +43,7 @@ run_analysis <- function() {
   ## Merge the X data, Y data and Subject data from the test and train sets
   X_Merged<-rbind(X_test, X_train)
   Y_Merged<-rbind(Y_test, Y_train)
-  subject_Merged<-rbind(subject_test, subject_train)
+  subject<-rbind(subject_test, subject_train)
   
   ## The column names are available in the features data
   ## Importing the features data and binding it with X_Merged
@@ -60,9 +64,49 @@ run_analysis <- function() {
   ## measures which are either have the string mean() or std(). We name this 
   ## subsetted dataframe as Mean_Std_Measures
   
-  Mean_Std_Measures<- X_Merged[,selmeasures]
+  Mean_Std_Measures <- X_Merged[,selmeasures]
   
+  ## Step 3 & 4: Uses descriptive activity names to name the activities in the 
+  ## data set AND Appropriately labels the data set with descriptive variable names
   
+  ## The activity labels are already present in a file called activity_labels
   
+  ## Reading in the Activity Labels file
+  
+  activity_labels <-read.table("./UCI HAR Dataset/activity_labels.txt")
+  
+  ## Converting the Y_Merged into factors
+  
+  Y_factor <- as.factor(Y_Merged[,1])
+  
+  ## Using the mapvalues function (from plyr package) to convert the Y_factors into
+  ## Activity_Names
+  
+  Activity_Names <- mapvalues(Y_factor,from = as.character(activity_labels[,1]), to = as.character(activity_labels[,2]))
+  
+  ## Including the Activity_Names as a column in the Mean_Std_Measures data frame
+  
+  Mean_Std_Measures <- cbind(Activity_Names, Mean_Std_Measures)  
+  
+  ## Also including the Subject ID's in the Mean_Std_Measures Dataframe
+  
+  Mean_Std_Measures <- cbind(subject, Mean_Std_Measures)
+  
+  ## So the Mean_Std_Measures dataframe has the following:
+  
+  ## 1st Column ----> Subject ID's
+  ## 2nd Column ----> Activity Names
+  ## Remaining columns ----> all measures containing mean or std deviation values
+  
+  ## Step 5: creates a second, independent tidy data set 
+  ##         with the average of each variable for each activity and each subject
+  
+  ## So the new tidy dataset should have all the Subject ID's, grouped by the activity names
+  ## like Walking, Walking_Upstairs etc followed by the average value of all the measures
+  
+  ## To do this we will use the reshape library, specifically using the melt and recast functions
+  
+  melt_stage <- melt(Mean_Std_Measures,id.vars=c("subject","Activity_Names"))
+  tidy_data <- dcast(melt_stage, subject + Activity_Names ~ variable, mean)
   
 }
