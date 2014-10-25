14dbf8b9a54012a9e9079cc8ed2691587d703844
devdutta <devjothi@gmail.com>
2014-10-25 21:29:27 +0530

    Importing Data - Completed

---
+++ run_analysis.R
@@ -0,0 +1,39 @@
+## This is the submission for the Course Project for Coursera- Getting 
+## and Cleaning Data
+
+## This Project is based on the Human Activity Recognition Using Smartphones Data Set 
+
+## This particular R Script does the following tasks:
+##    1) Merges the training and the test sets to create one data set.
+##    2) Extracts only the measurements on the mean and standard deviation
+##       for each measurement. 
+##    3) Uses descriptive activity names to name the activities in the data set
+##    4) Appropriately labels the data set with descriptive variable names. 
+##    5) From the data set in step 4, creates a second, independent tidy data set 
+##       with the average of each variable for each activity and each subject
+
+## Some assumptions if you are running this R Script
+
+##    1) This directory is already present --> "UCI HAR Dataset"
+##    2) All the relevant data files are also present within this directory
+##    3) Expecting that the "reshpae" package is installed. If not, then you will have to install it before
+##    4) running this script
+
+run_analysis <- function() {
+  
+    
+  ## Importing the Test Data
+  
+  X_test<-read.table("./UCI HAR Dataset/test/X_test.txt")
+  Y_test<-read.table("./UCI HAR Dataset/test/y_test.txt")
+  subject_test<-read.table("./UCI HAR Dataset/test/subject_test.txt")
+  
+  ## Import the training data
+  
+  X_train<-read.table("./UCI HAR Dataset/train/X_train.txt")
+  Y_train<-read.table("./UCI HAR Dataset/train/y_train.txt")
+  subject_train<-read.table("./UCI HAR Dataset/train/subject_train.txt")
+  
+  ## Step 1: Merges the training and the test sets to create one data set
+  
+}
